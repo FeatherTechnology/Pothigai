4000df635fbd1a261a19e647f8916e163dbdba09
akshaya <akshayaashok331@gmail.com>
2025-02-11 09:48:32 +0530

    Auction Screen
    Correction on auction Screen - Completed

--- api/auction_files/get_customerName_list.php
+++ api/auction_files/get_customerName_list.php
@@ -13,78 +13,78 @@ if (isset($group_id) && !empty($group_id) && isset($auction_month) && !empty($au
         SELECT gs.cus_id
         FROM auction_details ad
         LEFT JOIN group_share gs ON ad.cus_name = gs.cus_mapping_id
-        WHERE group_id = '$group_id' AND gs.settle_status = 'Yes'
+        WHERE group_id = '$group_id' 
         AND auction_month < '$auction_month'
     ";
+    
     $taken_customers = $pdo->query($taken_auction_qry)->fetchAll(PDO::FETCH_COLUMN);
 
     // Get the list of customer names already in other_transaction for this group
     $transaction_qry = "
     SELECT group_mem 
     FROM other_transaction 
-    WHERE group_id = '$group_id' GROUP BY group_mem;
+    WHERE group_id = '$group_id' AND type =2;
 ";
     $transaction_customers = $pdo->query($transaction_qry)->fetchAll(PDO::FETCH_COLUMN);
 
     // Get eligible customers for the current auction month
-    $qry = "
-        (
-            SELECT 
-                gcm.id,
-                gs.cus_id as cust_id,
-                cc.cus_id, 
-                GROUP_CONCAT(DISTINCT cc.first_name SEPARATOR ' - ') AS cus_name,
-                pl.place,
-                COUNT(DISTINCT CASE 
-                                WHEN (SELECT COUNT(*) 
-                                      FROM group_share gs_check 
-                                      WHERE gs_check.cus_mapping_id = gs.cus_mapping_id AND gs_check.share_percent = 100) = 1 
-                                THEN gs.cus_mapping_id
-                                ELSE NULL 
-                              END) AS chit_count
-            FROM 
-                group_share gs
-            JOIN 
-                customer_creation cc ON gs.cus_id = cc.id
-            JOIN 
-                place pl ON cc.place = pl.id 
-            JOIN 
-                group_cus_mapping gcm ON gs.cus_mapping_id = gcm.id
-            WHERE 
-                gs.grp_creation_id = '$group_id' 
-                AND gcm.joining_month <= '$auction_month' AND gs.share_percent = 100
-            GROUP BY 
-                cc.cus_id
-            HAVING 
-                chit_count > 0
-        )
-        UNION ALL
-        (
-            SELECT 
-                gcm.id,
-                GROUP_CONCAT(DISTINCT gs.cus_id) AS cust_id,
-                '' AS cus_id, 
-                GROUP_CONCAT(cc.first_name SEPARATOR ' - ') AS cus_name,
-                '' AS place,
-                (SELECT COUNT(*) 
-                 FROM group_share gs_sub
-                 WHERE gs_sub.cus_id = gs.cus_id AND gs_sub.grp_creation_id = '$group_id') AS chit_count
-            FROM 
-                group_share gs
-            JOIN 
-                customer_creation cc ON gs.cus_id = cc.id
-            JOIN 
-                group_cus_mapping gcm ON gs.cus_mapping_id = gcm.id
-            WHERE 
-                gs.grp_creation_id = '$group_id' 
-                AND gcm.joining_month <= '$auction_month'
-            GROUP BY 
-                gcm.id
-            HAVING 
-                COUNT(*) > 1
-        );
-    ";
-
+     $qry = "
+    (
+        SELECT 
+            gcm.id,
+            gs.cus_id as cust_id,
+            cc.cus_id, 
+            GROUP_CONCAT(DISTINCT cc.first_name SEPARATOR ' - ') AS cus_name,
+            pl.place,
+            COUNT(DISTINCT CASE 
+                            WHEN (SELECT COUNT(*) 
+                                  FROM group_share gs_check 
+                                  WHERE gs_check.cus_mapping_id = gs.cus_mapping_id AND gs_check.share_percent = 100) = 1 
+                            THEN gs.cus_mapping_id
+                            ELSE NULL 
+                          END) AS chit_count
+        FROM 
+            group_share gs
+        JOIN 
+            customer_creation cc ON gs.cus_id = cc.id
+        JOIN 
+            place pl ON cc.place = pl.id 
+        JOIN 
+            group_cus_mapping gcm ON gs.cus_mapping_id = gcm.id
+        WHERE 
+            gs.grp_creation_id = '$group_id' 
+            AND gcm.joining_month <= '$auction_month' AND gs.share_percent = 100
+        GROUP BY 
+            cc.cus_id
+        HAVING 
+            chit_count > 0
+    )
+    UNION ALL
+    (
+        SELECT 
+            gcm.id,
+            GROUP_CONCAT(DISTINCT gs.cus_id) AS cust_id,
+            '' AS cus_id, 
+            GROUP_CONCAT(cc.first_name SEPARATOR ' - ') AS cus_name,
+            '' AS place,
+            (SELECT COUNT(*) 
+             FROM group_share gs_sub
+             WHERE gs_sub.id = gs.id AND gs_sub.grp_creation_id = '$group_id') AS chit_count
+        FROM 
+            group_share gs
+        JOIN 
+            customer_creation cc ON gs.cus_id = cc.id
+        JOIN 
+            group_cus_mapping gcm ON gs.cus_mapping_id = gcm.id
+        WHERE 
+            gs.grp_creation_id = '$group_id' 
+            AND gcm.joining_month <= '$auction_month'
+        GROUP BY 
+            gcm.id
+        HAVING 
+            COUNT(*) > 1
+    );
+";
     $customers = $pdo->query($qry)->fetchAll(PDO::FETCH_ASSOC);
 
     // Filter customers based on their chit count, auction participation, and transactions in the other_transaction table
